de9f72771315da457a3f2882920f8828f3eeb3a0
Lee Ho Sung <2469766+leehosung@users.noreply.github.com>
2021-09-05 13:40:27 +0900

    traces_sample_rate 추가 캡춰하기 좋은 포멧으로 변경

--- data_scrubbing_before_send.py
+++ data_scrubbing_before_send.py
@@ -4,9 +4,10 @@ from sentry_sdk import set_tag
 
 
 def before_send(event, hint):
-    event["tags"]["birthday"] = hashlib.md5(event["tags"]["birthday"].encode()).hexdigest()
+    event["tags"]["birthday"] = hashlib.md5(
+        event["tags"]["birthday"].encode()
+    ).hexdigest()
     return event
-
 
 sentry_sdk.init(
     "https://3bb1bdb8bd5847afb2eaa62f7af6aa34@o987609.ingest.sentry.io/5944553",
--- data_scrubbing_set_user.py
+++ data_scrubbing_set_user.py
@@ -3,9 +3,11 @@ from sentry_sdk import set_user
 
 sentry_sdk.init(
     "https://3bb1bdb8bd5847afb2eaa62f7af6aa34@o987609.ingest.sentry.io/5944553",
+    traces_sample_rate=1.0,
 )
 
-set_user({"email": "sentry@pycon.kr", "name": "lee ho sung"})
+set_user({"email": "sentry@pycon.kr",
+          "name": "lee ho sung"})
 
 # 개인정보가 포함되어 있지 않은 정보로 변경
 set_user({"id": 14253})
