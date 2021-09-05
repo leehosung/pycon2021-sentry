38b9b7210dd95c14335066e04c5c8e10de4840f3
Lee Ho Sung <2469766+leehosung@users.noreply.github.com>
2021-09-05 12:43:55 +0900

    간단하게 수정

--- step1.py
+++ step1.py
@@ -2,7 +2,6 @@ import sentry_sdk
 
 sentry_sdk.init(
     "https://3bb1bdb8bd5847afb2eaa62f7af6aa34@o987609.ingest.sentry.io/5944553",
-    traces_sample_rate=1.0
 )
 
-division_by_zero = 2 / 0
+division_by_zero = 1 / 0
